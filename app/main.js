e78b34e4b224567c640b62a6ffb377fb8f25f72b
jo4o252000 <joaovictor252000@gmail.com>
2024-09-14 20:22:44 -0300

    initial commit

---
+++ app/main.js
@@ -0,0 +1,12 @@
+let livros  = []
+const endPointApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosApi()
+
+
+async function getBuscarLivrosApi(){
+    const res = await fetch(endPointApi)
+    livros = await res.json()
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirLivrosNaTela(livrosComDesconto)
+}
+
---
+++ app/metodoForEach.js
@@ -0,0 +1,32 @@
+const elementoParaInserirLivro = document.getElementById('livros')
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis')
+
+function exibirLivrosNaTela(listaDeLivros){
+    elementoParaInserirLivro.innerHTML = ''
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
+    listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livros__imagens indisponivel'
+        elementoParaInserirLivro.innerHTML += `
+        <div class="livro">
+            <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
+            <h2 class="livro__titulo">
+            ${livro.titulo}
+            </h2>
+            <p class="livro__descricao">${livro.autor}</p>
+            <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+            <div class="tags">
+            <span class="tag">${livro.categoria}</span>
+            </div>
+        </div>
+        `
+    })
+}
+
+// function verificarDisponibilidadeDoLivro(livro){
+//     if(livro.quantidade > 0){
+//         return 'livro__imagens'
+//     }else{
+//         return 'livros__imagens indisponivel'
+//     }
+// }
